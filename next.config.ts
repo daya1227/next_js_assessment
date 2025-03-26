ed1055c61e499d7ff84e68347a491df77243cb58
K Dayaker <k.dayaker@PFIN.COM>
2025-03-26 12:05:34 +0530

    iisue

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  trailingSlash: true,
-  basePath: ""
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Layout from '../components/Layout';
-
+import Layout from '../components/Layout'; // Ensure this path is correct
 
 export default function Home() {
   return (
